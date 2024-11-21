191e5cfbff8c7081d2f99042f184d652cec04bdd
Mario Agudelo <37761296-marioagudelo91@users.noreply.replit.com>
2024-11-21 00:13:27 +0000

    se agrega metodo get

--- docker/tercera-aplicacion/app.py
+++ docker/tercera-aplicacion/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import boto3
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('tabla-alexanderagudelo')
 
 
+#insertar información en base de datos
 @app.route('/insert', methods=['POST'])
 def index():
   data = request.json
@@ -14,6 +15,21 @@ def index():
   tabla.put_item(Item=item)
 
   return 'Se guardó exitosamente la información'
+
+
+#leer información de base de datos
+@app.route('/get/<id>', methods=['GET'])
+def get_item(id):
+  try:
+    response = tabla.get_item(Key={'id': id})
+
+    if 'Item' in response:
+      return jsonify(response['Item']), 200
+    else:
+      return jsonify({'error': 'No se encontró el item'}), 404
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
